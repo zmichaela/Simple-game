34b0a21c42874ed0171b5eec8f359449ba4b6c02
Michaela <44002738+zmichaela@users.noreply.github.com>
2020-05-05 13:06:23 +0100

    updated tags to english, added music

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-/*
-ZADÁNÍ DÚ:
-1) při změně směru změň obrázek panáčka;
-2) když se srazí panáček s mincí, mince se přesune na jinou, náhodně vygenerovanou pozici (pozor na omezení šířky a výšky okna!)
-3) vytvoř proměnnou, ve které se bude zvyšovat skóre po setkání panáčka a mince. Toto skóre se bude zobrazovat na stránce
-
-BONUS) doplňte hru o zvuky :)
-*/
-
-
-let man = document.querySelector('#man');
+let man = document.querySelector("#man");
 let manX = 400;
 let manY = 300;
 let manHeight = man.height;
 let manWidth = man.width; 
-let coin = document.querySelector('#coin');
+let coin = document.querySelector("#coin");
 let coinX = 200;
 let coinY = 250;
 let coinHeight = coin.height;
@@ -27,63 +17,63 @@ let fanfara = document.querySelector("#fanfara");
 let scoreCount = 0;
 
 //Umistime panacka na stranku
-function placeMan(){
-	man.style.left = manX + 'px';
-	man.style.top = manY + 'px';
+function placeMan() {
+	man.style.left = manX + "px";
+	man.style.top = manY + "px";
 }
 placeMan();
 
 
 //Umistime minci na stranku
-function umistiMinci() {
-	mince.style.left = minceX + 'px';
-	mince.style.top = minceY + 'px';	
+function placeCoin() {
+	coin.style.left = coinX + "px";
+	coin.style.top = coinY + "px";	
 }
-umistiMinci();
+placeCoin();
 
 
 //Zmena pozice mince - nahodne souradnice
-function premistiMinci () {
+function moveCoin () {
 	let newWidth = Math.floor(Math.random() * window.innerWidth);
 	let newHeight = Math.floor(Math.random() * window.innerHeight);
-	mince.style.left = newWidth + "px";
-	mince.style.top = newHeight + "px";
+	coin.style.left = newWidth + "px";
+	coin.style.top = newHeight + "px";
 }
 
 // pocitadlo
 function pocitadlo () {
-    skoreCount += 1;
+    scoreCount += 1;
     scoreDiv.textContent = scoreCount;
-    premistiMinci();
+    moveCoin();
 }
 
 
 // Pohybujeme panackem
-function movingKeyboard(udalost){
-	let klavesa = udalost.key;
-	if (klavesa === 'ArrowRight'){
-        panacekX += 10;		
-        panacek.src = "obrazky/panacek-vpravo.png";
-		if (manX > window.innerWidth - manSirka){
-			manX = window.innerWidth - manSirka;
+function movingKeyboard(event) {
+	let keyboard = event.key;
+	if (keyboard === 'ArrowRight'){
+        manX += 10;		
+        man.src = "images/panacek-vpravo.png";
+		if (manX > window.innerWidth - manWidth){
+			manX = window.innerWidth - manWidth;
 		}
-	} else if (klavesa === 'ArrowLeft'){
+	} else if (keyboard === 'ArrowLeft') {
         manX -= 10;	
-        man.src = "obrazky/panacek-vlevo.png";	
+        man.src = "images/panacek-vlevo.png";	
 		if (manX < 0){
 			manX = 0;
 		}
-	} else if (klavesa === 'ArrowUp'){
+	} else if (keyboard === 'ArrowUp') {
         manY = manY - 10;
-        man.src = "obrazky/panacek-nahoru.png";
+        man.src = "images/panacek-nahoru.png";
 		if (manY < 0){
 			manY = 0;
 		}
-	} else if (klavesa === 'ArrowDown'){
+	} else if (keyboard === 'ArrowDown') {
 		manY += 10;	
-        man.src = "obrazky/panacek.png";
-		if (manY > window.innerHeight - manVyska){
-			manY = window.innerHeight - manVyska;
+        man.src = "images/panacek.png";
+		if (manY > window.innerHeight - manHeight){
+			manY = window.innerHeight - manHeight;
 		}
     } 
     placeMan();
@@ -92,12 +82,18 @@ function movingKeyboard(udalost){
 	manY + manHeight < coinY || coinY + coinHeight < manY)) {
         coinSound.play();
         pocitadlo();
-        if (skore === 5) {
+        if (score === 5) {
           fanfara.play();  
-          scoreDiv.textContent = "You won";
+          scoreDiv.textContent = "You won this game. Well done!";
 	} 
     music.play();
+  }
 }
-}
+/*
+ZADÁNÍ DÚ:
+1) při změně směru změň obrázek panáčka;
+2) když se srazí panáček s mincí, mince se přesune na jinou, náhodně vygenerovanou pozici (pozor na omezení šířky a výšky okna!)
+3) vytvoř proměnnou, ve které se bude zvyšovat skóre po setkání panáčka a mince. Toto skóre se bude zobrazovat na stránce
 
-//bohuzel se mi to nepovedlo dotahnout uplne do konce, hra pokracuje po dosazeni peti bodu a mince trochu blbne tak prosim o radu
+BONUS) doplňte hru o zvuky :)
+*/
